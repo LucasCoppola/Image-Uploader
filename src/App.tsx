f96baf616ae30e0548546358ac80f1be8235862e
Lucas Coppola <112649792+LucasCoppola@users.noreply.github.com>
2023-07-07 11:36:17 -0300

    styles

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,16 @@ import DragDrop from './Drag&Drop'
 const App = () => {
 	return (
 		<div className="h-screen flex flex-col justify-center items-center relative">
-			<div className="w-96 h-96 mx-auto px-8 py-4 bg-white rounded-lg shadow-md flex flex-col items-center">
-				<h1>Upload your image</h1>
-				<h4>Image should be Jpeg, Png...</h4>
-				<div className="flex items-center justify-center mt-4">
-					<DragDrop />
+			<div className="w-96 h-[calc(100vh-170px)] mx-auto bg-white rounded-xl shadow-[0_3px_10px_rgb(0,0,0,0.2)] overflow-hidden">
+				<div className="px-8 py-4 flex flex-col items-center">
+					<h1 className="font-semibold text-gray-700 text-xl my-4">Upload your image</h1>
+					<h4 className="text-gray-500 text-xs">File should be Jpeg, Png...</h4>
+					<div className="flex items-center justify-center mt-4">
+						<DragDrop />
+					</div>
+					<p className="text-gray-500 text-xs my-4">Or</p>
+					<button className="bg-blue-600 mb-3 text-white px-4 py-2 rounded-lg text-sm">Choose a File</button>
 				</div>
-				<p>Or</p>
-				<button>Choose a File</button>
 			</div>
 			<footer className="text-gray-500 text-xs absolute bottom-5">created by Lucas Coppola - devChallenges.io</footer>
 		</div>
--- src/Drag&Drop.tsx
+++ src/Drag&Drop.tsx
@@ -2,14 +2,14 @@ import SvgImage from './SvgImage'
 
 const DragDrop = () => {
 	return (
-		<div className="bg-blue-50 flex items-center justify-center">
+		<div className="bg-[#f4fdff] flex items-center justify-center rounded-lg">
 			<label
 				htmlFor="dropzone-file"
-				className="flex w-80 flex-col items-center justify-center h-52 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer hover:bg-gray-100"
+				className="flex w-80 flex-col items-center justify-center h-52 border-2 border-blue-100 border-dashed rounded-lg cursor-pointer hover:bg-blue-50"
 			>
 				<div className="flex flex-col items-center justify-center pt-5 pb-6">
 					<SvgImage />
-					<p className="text-xs text-gray-500">Drag & Drop your image here</p>
+					<p className="text-xs text-gray-500 mt-8">Drag & Drop your image here</p>
 				</div>
 				<input id="dropzone-file" type="file" className="hidden" />
 			</label>
