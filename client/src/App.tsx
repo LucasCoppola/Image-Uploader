870db6382902977f5863102c320d1beb8def76ef
Lucas Coppola <112649792+LucasCoppola@users.noreply.github.com>
2023-07-09 14:16:11 -0300

    trying to make the form work

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,20 @@
+import { useRef } from 'react'
 import DragDrop from './Drag&Drop'
 
 const App = () => {
+	const fileInputRef = useRef<HTMLInputElement | undefined>(null)
+
+	const handleFileButtonClick = () => {
+		if (fileInputRef.current) {
+			fileInputRef.current.click()
+		}
+	}
+
+	const handleFileInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const file = e.target.files
+		// Do something with the selected file
+		console.log(file)
+	}
 	return (
 		<div className="h-screen flex flex-col justify-center items-center relative">
 			<div className="w-96 h-[calc(100vh-170px)] mx-auto bg-white rounded-xl shadow-[0_3px_10px_rgb(0,0,0,0.2)] overflow-hidden">
@@ -11,7 +25,18 @@ const App = () => {
 						<DragDrop />
 					</div>
 					<p className="text-gray-500 text-xs my-4">Or</p>
-					<button className="bg-blue-600 mb-3 text-white px-4 py-2 rounded-lg text-sm">Choose a File</button>
+					<form action="/upload" method="post" encType="multipart/form-data">
+						<input
+							type="file"
+							name="avatar"
+							ref={fileInputRef}
+							style={{ display: 'none' }} // Hide the default file input
+							onChange={handleFileInputChange}
+						/>
+					</form>
+					<button className="bg-blue-600 mb-3 text-white px-4 py-2 rounded-lg text-sm" onClick={handleFileButtonClick}>
+						Choose a File
+					</button>
 				</div>
 			</div>
 			<footer className="text-gray-500 text-xs absolute bottom-5">created by Lucas Coppola - devChallenges.io</footer>
