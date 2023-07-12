d30c5918e4d743d1e206dca9c7b2751af2d36069
Lucas Coppola <112649792+LucasCoppola@users.noreply.github.com>
2023-07-12 18:53:00 -0300

    copy to clipboard and styles

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,7 @@ import SuccessCard from "./SuccessCard"
 const App = () => {
 	const [image, setImage] = useState<File | null>(null)
 	const [isLoading, setIsLoading] = useState(false)
+	const [isUploaded, setIsUploaded] = useState(false)
 	const fileInputRef = useRef<HTMLInputElement | null>(null)
 
 	const handleFileButtonClick = () => {
@@ -49,41 +50,42 @@ const App = () => {
 				</div>
 			</div>
 		</div>
-	) : (
+	) : isUploaded ? (
 		<SuccessCard />
-		// <div className="h-screen flex flex-col justify-center items-center relative">
-		// 	<div className="w-96 h-[calc(100vh-170px)] mx-auto bg-white rounded-xl shadow-[0_3px_10px_rgb(0,0,0,0.2)] overflow-hidden">
-		// 		<div className="px-8 py-4 flex flex-col items-center">
-		// 			<h1 className="font-semibold text-gray-700 text-xl my-4">Upload your image</h1>
-		// 			<h4 className="text-gray-500 text-xs">File should be Jpeg, Png...</h4>
-		// 			<div className="flex items-center justify-center mt-4">
-		// 				<DragDrop />
-		// 			</div>
-		// 			<img src={image ? URL.createObjectURL(image) : ""} alt={image?.name} />
-		// 			<p className="text-gray-500 text-xs my-4">Or</p>
-		// 			<form action="/upload" method="post" encType="multipart/form-data">
-		// 				<input
-		// 					type="file"
-		// 					name="image"
-		// 					ref={fileInputRef}
-		// 					accept="image/png, image/jpeg"
-		// 					style={{ display: "none" }}
-		// 					onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-		// 						handleFileInputChange(e).catch((e) => console.error(e))
-		// 					}}
-		// 				/>
-		// 			</form>
-		// 			<button
-		// 				className="bg-blue-600 mb-3 text-white px-4 py-2 rounded-lg text-sm"
-		// 				onClick={handleFileButtonClick}>
-		// 				Choose a File
-		// 			</button>
-		// 		</div>
-		// 	</div>
-		// 	<footer className="text-gray-500 text-xs absolute bottom-5">
-		// 		created by Lucas Coppola - devChallenges.io
-		// 	</footer>
-		// </div>
+	) : (
+		<div className="h-screen flex flex-col justify-center items-center relative">
+			<div className="w-96 h-[calc(100vh-170px)] mx-auto bg-white rounded-xl shadow-[0_3px_10px_rgb(0,0,0,0.2)] overflow-hidden">
+				<div className="px-8 py-4 flex flex-col items-center">
+					<h1 className="font-semibold text-gray-700 text-xl my-4">Upload your image</h1>
+					<h4 className="text-gray-500 text-xs">File should be Jpeg, Png...</h4>
+					<div className="flex items-center justify-center mt-4">
+						<DragDrop />
+					</div>
+					<img src={image ? URL.createObjectURL(image) : ""} alt={image?.name} />
+					<p className="text-gray-500 text-xs my-4">Or</p>
+					<form action="/upload" method="post" encType="multipart/form-data">
+						<input
+							type="file"
+							name="image"
+							ref={fileInputRef}
+							accept="image/png, image/jpeg"
+							style={{ display: "none" }}
+							onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+								handleFileInputChange(e).catch((e) => console.error(e))
+							}}
+						/>
+					</form>
+					<button
+						className="bg-blue-600 mb-3 text-white px-4 py-2 rounded-lg text-sm"
+						onClick={handleFileButtonClick}>
+						Choose a File
+					</button>
+				</div>
+			</div>
+			<footer className="text-gray-500 text-xs absolute bottom-5">
+				created by Lucas Coppola - devChallenges.io
+			</footer>
+		</div>
 	)
 }
 
--- client/src/SuccessCard.tsx
+++ client/src/SuccessCard.tsx
@@ -1,11 +1,40 @@
 const SuccessCard = () => {
+	const inputValue = "https://res.cloudinary.com/dotpfjpno/image/upload/v1675864626/YelpCamp/yrwrxu55rg5bdfgyrzcc.jpg"
+
+	const copy = () => {
+		navigator.clipboard
+			.writeText(inputValue)
+			.then(() => console.log("Copied!"))
+			.catch((err) => console.error("Failed to copy text: ", err))
+	}
+
 	return (
 		<div className="h-screen flex flex-col justify-center items-center relative">
-			<div className="w-96 h-[calc(100vh-170px)] mx-auto bg-white rounded-xl shadow-[0_3px_10px_rgb(0,0,0,0.2)] overflow-hidden">
-				<div className="flex items-center justify-center h-16 mt-5">
-					<div className="w-20 h-20 flex items-center justify-center rounded-full bg-green-600">
-						<span className="material-icons text-white text-5xl">done</span>
+			<div className="w-96 py-8 bg-white rounded-xl shadow-[0_3px_10px_rgb(0,0,0,0.2)] overflow-hidden flex flex-col justify-center items-center">
+				<div className="flex items-center justify-center h-16 ">
+					<div className="w-12 h-12 flex items-center justify-center rounded-full bg-green-600">
+						<span className="material-icons text-white text-4xl">done</span>
 					</div>
+				</div>
+				<h1 className="font-semibold text-gray-700 text-center my-1 text-xl">Uploaded Successfully!</h1>
+				<img
+					src="https://res.cloudinary.com/dotpfjpno/image/upload/v1675864626/YelpCamp/yrwrxu55rg5bdfgyrzcc.jpg"
+					alt=""
+					className="w-80 rounded-2xl my-4"
+				/>
+
+				<div className="relative flex items-center mt-2">
+					<input
+						type="text"
+						value={inputValue}
+						className="border p-3 pr-24 w-80 rounded-lg text-gray-800 bg-blue-50 text-xs flex items-center truncate"
+						readOnly
+					/>
+					<button
+						onClick={copy}
+						className="absolute right-1 rounded-lg py-2 px-3 bg-blue-500 text-white text-xs flex items-center justify-center">
+						Copy Link
+					</button>
 				</div>
 			</div>
 			<footer className="text-gray-500 text-xs absolute bottom-5">
